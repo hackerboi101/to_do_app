71a27e3020c28ca3a40a53a3f222a9dd609d5ed7
hackerboi101 <asif.iq.024@gmail.com>
2023-02-17 23:09:33 +0600

    Finished project

--- lib/Utilities/dialog_box.dart
+++ lib/Utilities/dialog_box.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, must_be_immutable, prefer_typing_uninitialized_variables
 
 import 'package:flutter/material.dart';
 import 'package:to_do_app/Utilities/my_button.dart';
